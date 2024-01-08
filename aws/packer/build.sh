dbc9c330e216e2b06392fcf7d78759746731d136
Mark D'Ascenzo <mark.dascenzo@gmail.com>
2024-01-08 10:22:02 -0800

    add build timestamp

--- aws/packer/build.sh
+++ aws/packer/build.sh
@@ -1,1 +1,14 @@
-packer build ami-rnaseq.hpc &> logs/build.log.tmp &
+#!/bin/bash
+
+# create a timestamp for the build
+BUILD_TIMESTAMP=$(date +%Y%m%d-%H%M%S)
+
+# export logging variables
+export PACKER_LOG=1
+export PACKER_LOG_PATH="logs/packer-detailed-$BUILD_TIMESTAMP.log"
+
+# create logs directory if it doesn't exist
+[ ! -d "logs" ] && mkdir -p "logs"
+
+# build the AMI
+packer build -var "build_timestamp=$BUILD_TIMESTAMP" -var-file=build-variables.json ami-rnaseq.json
